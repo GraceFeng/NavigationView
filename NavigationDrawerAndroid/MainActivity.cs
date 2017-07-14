1d75260883aeab640b21bc783a45bc88f90c716a
v-linfen <v-linfen@GRACE-PC>
2017-07-14 15:04:59 +0800

    Add project files.

---
+++ NavigationDrawerAndroid/MainActivity.cs
@@ -0,0 +1,45 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Support.Design.Widget;
+using Android.Support.V4.Widget;
+
+namespace NavigationDrawerAndroid
+{
+    [Activity(Label = "NavigationDrawerAndroid", MainLauncher = true, Icon = "@drawable/icon", Theme = "@style/MyTheme")]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            FragmentTransaction transaction = this.FragmentManager.BeginTransaction();
+            HomeFragment home = new HomeFragment();
+            transaction.Add(Resource.Id.framelayout, home).Commit();
+
+            var drawerLayout = FindViewById<DrawerLayout>(Resource.Id.drawer_layout);
+            var naviview = FindViewById<NavigationView>(Resource.Id.nav_view);
+            naviview.NavigationItemSelected += (sender, e) =>
+            {
+                e.MenuItem.SetChecked(true);
+                FragmentTransaction transaction1 = this.FragmentManager.BeginTransaction();
+                switch (e.MenuItem.TitleFormatted.ToString())
+                {
+                    case "Home":
+                        transaction1.Replace(Resource.Id.framelayout, home).AddToBackStack(null).Commit();
+                        break;
+
+                    case "Video":
+                        VideoFragment video = new VideoFragment();
+                        transaction1.Replace(Resource.Id.framelayout, video).AddToBackStack(null).Commit();
+                        break;
+                }
+                drawerLayout.CloseDrawers();
+            };
+        }
+    }
+}
---
+++ NavigationDrawerAndroid/VideoFragment.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Util;
+using Android.Views;
+using Android.Widget;
+using Android.Media;
+using static Android.Media.MediaPlayer;
+
+namespace NavigationDrawerAndroid
+{
+    public class VideoFragment : Fragment, IOnPreparedListener
+    {
+        private VideoView videoView;
+        private MediaPlayer mediaPlayer;
+        private MediaController mediaController;
+
+        public override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your fragment here
+        }
+
+        public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+            // Use this to return your custom view for this Fragment
+            var view = inflater.Inflate(Resource.Layout.videolayout, container, false);
+            videoView = view.FindViewById<VideoView>(Resource.Id.videoview);
+            videoView.Prepared += VideoView_Prepared;
+            mediaController = new MediaController(this.Activity, true);
+            return view;
+        }
+
+        private void StartVideo()
+        {
+            mediaController.SetAnchorView(videoView);
+            mediaController.SetMediaPlayer(videoView);
+            String fileName = "android.resource://" + this.Activity.BaseContext.PackageName + "/raw/one";
+            videoView.SetVideoURI(Android.Net.Uri.Parse(fileName));
+            videoView.Start();
+        }
+
+        private void VideoView_Prepared(object sender, EventArgs e)
+        {
+            mediaController.Show(2000);
+        }
+
+        public override void OnStart()
+        {
+            base.OnStart();
+            StartVideo();
+            videoView.SetOnPreparedListener(this);
+            videoView.Touch += (sender, e) =>
+            {
+                if (!(e.Event.Action == MotionEventActions.Down))
+                    return;
+                if (!mediaController.IsShown)
+                    mediaController.Show();
+                else
+                    mediaController.Hide();
+            };
+
+            videoView.Completion += (sender, e) =>
+            {
+                //base.OnBackPressed();
+            };
+        }
+
+        public override void OnStop()
+        {
+            base.OnStop();
+            videoView.Prepared -= VideoView_Prepared;
+        }
+
+        public void OnPrepared(MediaPlayer mp)
+        {
+        }
+    }
+}
